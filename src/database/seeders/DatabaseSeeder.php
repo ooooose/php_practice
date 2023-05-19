c051731694f1f1ab24a1109a559f10add4f6730f
ooooose <yuuki.oose1990@gmail.com>
2023-05-19 12:04:26 +0900

    [Add]ダミーデータの生成

--- src/database/seeders/DatabaseSeeder.php
+++ src/database/seeders/DatabaseSeeder.php
@@ -14,5 +14,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        \App\Models\Department::factory(30)->create();
     }
 }

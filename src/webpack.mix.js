3764493ae2e6eb0a53c151152f263a004de27f21
ooooose <yuuki.oose1990@gmail.com>
2023-05-23 12:21:29 +0900

    [Add]tailwindcssの導入（成功）

--- src/webpack.mix.js
+++ src/webpack.mix.js
@@ -1,17 +1,14 @@
 const mix = require('laravel-mix');
+const tailwindcss = require('tailwindcss');//追記
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
- | file for the application as well as bundling up all the JS files.
- |
- */
 
- mix.js('resources/js/app.js', 'public/js')
- .postCss('resources/css/app.css', 'public/css', [
-     //
- ]);
+
+mix.sass('resources/sass/app.scss', 'public/css')
+    .options({ 
+        processCssUrls: false, //公式ドキュメント参照
+        postCss: [ tailwindcss('./tailwind.config.js') ], 
+    }); 
+
+if (mix.inProduction()) {
+    mix.version();
+}

8b37e48700ab9226ae9a549bac60b1872665bfb5
ooooose <yuuki.oose1990@gmail.com>
2023-05-29 15:13:59 +0900

    [Add]table名を明示（コーディングルール適用）

--- src/app/Models/Department.php
+++ src/app/Models/Department.php
@@ -9,6 +9,8 @@ class Department extends Model
 {
     use HasFactory;
 
+    protected $table = "departments";
+
     public function contact()
     {
         return $this->hasMany(Contact::class); 
